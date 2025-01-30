af732df8f608d63b275b1843fbed9559cbc6fb01
zhdanila <daniazhurbaaa@gmail.com>
2025-01-30 17:37:09 +0200

    Some improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,13 @@
 export GOOSE_DRIVER=postgres
 export GOOSE_DBSTRING="host=postgres port=5432 user=${DB_USERNAME} password=${DB_PASSWORD} dbname=${DB_NAME}"
 
-# Виконуємо міграції
 echo "Running migrations..."
 goose -dir /app/migrations up
 
-# Перевірка статусу міграцій
 if [ $? -ne 0 ]; then
   echo "Migrations failed!"
   exit 1
 fi
 
-# Тепер запускаємо сервер
 echo "Starting application..."
 ./main
